f4db067fe5164ac6d3e8c8953640b45457ff912a
hasbihz24 <hasbihz24@gmail.com>
2022-04-20 09:26:04 +0700

    Initial commit

---
+++ Karyawan.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman21._11._4411
+{
+     class Karyawan
+    {
+        public string Nik { get; set; }
+        public string Nama { get; set; }
+        public int GajiBulanan { get; set; }
+
+        public Karyawan(string nik,string nama,int gajibulanan)
+        {
+            Nik = nik;
+            Nama = nama;
+            GajiBulanan = gajibulanan;
+            if (gajibulanan < 0)
+            {
+                gajibulanan = 0;
+            }
+
+        }
+        public int naikGaji(int gaji)
+        {
+            return gaji = GajiBulanan + (GajiBulanan / 100 * 10);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman21._11._4411
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawan = new Karyawan("190302123", "Paijo", 3000000 );
+            Karyawan karyawan2 = new Karyawan("190302124", "Jono", 2000000);
+            Console.WriteLine("No NIK/Nama          Gaji Bulanan");
+            Console.WriteLine("_________________________________");
+            Console.WriteLine(" ");
+            Console.WriteLine("1. "+karyawan.Nik+" "+karyawan.Nama+"      "+karyawan.GajiBulanan);
+            Console.WriteLine("2. "+karyawan2.Nik+" "+karyawan2.Nama+"       "+karyawan2.GajiBulanan);
+            int naik = karyawan.naikGaji(karyawan.GajiBulanan);
+            int naik2 = karyawan2.naikGaji(karyawan2.GajiBulanan);
+            Console.WriteLine("");
+            Console.WriteLine("Kenaikan Gaji 10% ");
+            Console.WriteLine("No NIK/Nama          Gaji Bulanan");
+            Console.WriteLine("");
+            Console.WriteLine("_________________________________");
+            Console.WriteLine("1. " + karyawan.Nik + " " + karyawan.Nama + "      " + naik);
+            Console.WriteLine("2. " + karyawan2.Nik + " " + karyawan2.Nama + "       " + naik2);
+                
+
+            Console.ReadKey();
+        }
+    }
+}
